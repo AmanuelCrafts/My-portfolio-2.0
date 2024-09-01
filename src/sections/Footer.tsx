d15f575e076cc34ffcf37b0bbf3348c688c48566
AmanuelCrafts <amanuelabebawfr@gmail.com>
2024-08-31 17:51:38 -0700

    Fix: Pointer Events

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -22,7 +22,7 @@ const links = [
 export const Footer = () => {
   return (
     <footer className="relative z-0 overflow-x-clip">
-      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
+      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10 pointer-events-none"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2024. All rights reserved.</div>
